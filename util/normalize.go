207392e87e8a99cf376f41d29e02a2055319ade6
Spindlyskit <josh.lavelle1@gmail.com>
2019-09-04 18:44:02 +0100

    Add text repair function

--- util/normalize.go
+++ util/normalize.go
@@ -4,6 +4,7 @@ import (
 	"log"
 	"regexp"
 	"strings"
+	"unicode"
 )
 
 // Normalize makes a string uppercase and removes non alphabetic characters
@@ -21,4 +22,23 @@ func Normalize(str string) string {
 // NormalizeB makes a byte slice uppercase and removes non alphabetic characters
 func NormalizeB(text []byte) []byte {
 	return []byte(Normalize(string(text)))
+}
+
+// RepairB reverts the changes made by a normalize by fixing case and adding punctuation
+func RepairB(text, oldText []byte) []byte {
+	var repairedText []byte
+	oldTextCounter := 0
+	for _, c := range oldText {
+		char := rune(text[oldTextCounter])
+		if c >= 'A' && c <= 'Z' {
+			repairedText = append(repairedText, byte(unicode.ToUpper(char)))
+			oldTextCounter++
+		} else if c >= 'a' && c <= 'z' {
+			repairedText = append(repairedText, byte(unicode.ToLower(char)))
+			oldTextCounter++
+		} else {
+			repairedText = append(repairedText, c)
+		}
+	}
+	return repairedText
 }
